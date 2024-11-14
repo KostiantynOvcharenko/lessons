dce7ce8cbcb0ef2b95e7fede675bcc41ddf839d5
Kostiantyn Ovcharenko <z101z202@gmail.com>
2024-11-14 09:58:57 +0200

    Lesson 16

--- lesson_16/js/script.js
+++ lesson_16/js/script.js
@@ -1,4 +1,5 @@
 window.onload = () =>{
+    console.log(screen.width);
     // burger-menu
     document.addEventListener("click", function (e) {
         const targetElement = e.target;
@@ -24,6 +25,11 @@ window.onload = () =>{
     },options)
     document.querySelectorAll('.-trans').forEach(target=>{
         observer.observe(target);
+        if(screen.width > 1023){
+        }
+        else{
+            // target.documentElement.classList.remove('-trans');
+        }
     })
 }
 
