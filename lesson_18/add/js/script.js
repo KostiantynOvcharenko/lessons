4096ae13321afaf934a6a55bb32a33e016a36a1c
Kostiantyn Ovcharenko <z101z202@gmail.com>
2024-12-09 20:36:10 +0200

    Lesson 18

---
+++ lesson_18/add/js/script.js
@@ -0,0 +1,34 @@
+window.onload = () =>{
+    // burger-menu
+    document.addEventListener("click", function (e) {
+        const targetElement = e.target;
+        if (targetElement.closest('.header__burger')) {
+            document.documentElement.classList.toggle('open-menu');
+            e.preventDefault();
+        }
+    })
+    //=================================================
+    const options ={
+        root: null,
+        rootMargin: '0px',
+        threshold: .5
+    }
+    const observer = new IntersectionObserver((entries, observer) => {
+        entries.forEach(entry=>{
+            if(entry.isIntersecting){
+                const trans = entry.target;
+                trans.classList.add('active')
+                observer.unobserve(trans);
+            }
+        })
+    },options)
+    document.querySelectorAll('.-trans').forEach(target=>{
+        observer.observe(target);
+        if(screen.width > 1023){
+        }
+        else{
+            // target.documentElement.classList.remove('-trans');
+        }
+    })
+}
+
